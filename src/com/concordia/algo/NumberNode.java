5ed41ad048426beeb2b048381e37642ba83c6ce5
Chetan Paliwal <chetanpaliwal22@gmail.com>
2020-04-13 20:34:11 -0400

    tree number of node recursive

---
+++ src/com/concordia/algo/NumberNode.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class NumberNode {
+ public int countNodes(TreeNode root) {
+
+  if (root == null) {
+   return 0;
+  }
+  return 1 + countNodes(root.left) + countNodes(root.right);
+ }
+
+}
