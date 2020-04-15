d5eb993f971ca255466334ffec1fa2fbf3d63e08
Chetan Paliwal <chetanpaliwal22@gmail.com>
2020-04-14 23:46:34 -0400

    Last stone solution

---
+++ src/com/concordia/algo/LastStone.java
@@ -0,0 +1,23 @@
+class LastStone {
+ public int lastStoneWeight(int[] stones) {
+  if (stones.length == 1) {
+   return stones[0];
+  }
+  Arrays.sort(stones);
+  while (stones[stones.length - 2] != 0) {
+
+
+   System.out.println(stones[stones.length - 1]);
+   if (stones[stones.length - 1] == stones[stones.length - 2]) {
+    stones[stones.length - 1] = 0;
+    stones[stones.length - 2] = 0;
+   } else {
+    stones[stones.length - 1] = stones[stones.length - 1] - stones[stones.length - 2];
+    stones[stones.length - 2] = 0;
+   }
+   Arrays.sort(stones);
+  }
+
+  return stones[stones.length - 1];
+ }
+}
