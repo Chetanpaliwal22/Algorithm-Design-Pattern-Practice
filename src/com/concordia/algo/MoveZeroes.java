97bde8a588910160264060d728cbb084e87bc2f4
Chetan Paliwal <chetanpaliwal22@gmail.com>
2020-04-17 20:58:57 -0400

    Move Zeroes java

---
+++ src/com/concordia/algo/MoveZeroes.java
@@ -0,0 +1,22 @@
+class MoveZeroes {
+ public void moveZeroes(int[] nums) {
+
+  int i = 0, j = 0;
+
+  while (i < nums.length && j < nums.length) {
+   if (nums[j] == 0) {
+    j++;
+   } else {
+    nums[i] = nums[j];
+    i++;
+    j++;
+   }
+  }
+
+  while (i < nums.length) {
+   nums[i] = 0;
+   i++;
+  }
+  // return nums;
+ }
+}
