b32a986849c5b984a8e37cc44a4d224e79f91cd9
Chetan Paliwal <chetanpaliwal22@gmail.com>
2020-04-17 21:14:46 -0400

    Best Time to Buy and Sell Stock II

---
+++ src/com/concordia/algo/SellPurchase.java
@@ -0,0 +1,20 @@
+class SellPurchase {
+ public int maxProfit(int[] prices) {
+
+  if (prices.length == 0) {
+   return 0;
+  }
+
+  int pur = prices[0], sell = 0, profit = 0;
+
+  for (int i = 1; i < prices.length; i++) {
+
+   sell = prices[i];
+   if (sell - pur > 0) {
+    profit += (sell - pur);
+   }
+   pur = sell;
+  }
+  return profit;
+ }
+}
