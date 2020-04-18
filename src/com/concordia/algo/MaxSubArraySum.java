fac86c19ee9c77ea0c6dd045d3a99014810febff
Chetan Paliwal <chetanpaliwal22@gmail.com>
2020-04-17 20:51:45 -0400

    Largest Sum Contiguous Subarray

---
+++ src/com/concordia/algo/MaxSubArraySum.java
@@ -0,0 +1,25 @@
+class MaxSubArraySum {
+ public int maxSubArray(int[] nums) {
+  if (nums.length == 0) {
+   return 0;
+  }
+
+  int maxhere = nums[0], maxcont = 0;
+
+  for (int i = 0; i < nums.length; i++) {
+
+   maxcont += nums[i];
+   if (nums[i] > maxcont) {
+    maxcont = nums[i];
+   }
+
+   if (maxhere < maxcont) {
+    maxhere = maxcont;
+   }
+
+   // System.out.println(maxcont);
+  }
+
+  return maxhere;
+ }
+}
